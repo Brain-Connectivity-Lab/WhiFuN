66e1f3685c008d43ec5db0bd14147c5e147237ff
pratik55 <40887772+pratik55@users.noreply.github.com>
2024-12-08 17:56:07 -0500

    Add files via upload

---
+++ whifun_functions/whifun_skullstrip.m
@@ -0,0 +1,19 @@
+function whifun_skullstrip(m_file,now_anat_path,name,skull_pre)
+
+matlabbatch{1}.spm.util.imcalc.input = {
+    [fullfile(m_file.folder, m_file.name) '']
+    [complete_filepath(fullfile(now_anat_path(1).folder, ['c1' name '.nii'])) '']
+    [complete_filepath(fullfile(now_anat_path(1).folder, ['c2' name '.nii'])) '']
+    [complete_filepath(fullfile(now_anat_path(1).folder, ['c3' name '.nii'])) '']};
+
+matlabbatch{1}.spm.util.imcalc.output = [skull_pre now_anat_path(1).name];
+matlabbatch{1}.spm.util.imcalc.outdir = {now_anat_path(1).folder};
+matlabbatch{1}.spm.util.imcalc.expression = 'i1.*((i2+i3+i4)>0.5)';
+matlabbatch{1}.spm.util.imcalc.var = struct('name', {}, 'value', {});
+matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
+matlabbatch{1}.spm.util.imcalc.options.mask = 0;
+matlabbatch{1}.spm.util.imcalc.options.interp = 1;
+matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
+
+spm('defaults', 'FMRI');
+spm_jobman('run', matlabbatch);
