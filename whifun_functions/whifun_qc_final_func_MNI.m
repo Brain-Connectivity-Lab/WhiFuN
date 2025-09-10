2fee348ffb8d016d9f21cbced2638dab0fbf6dfb
pratik55 <40887772+pratik55@users.noreply.github.com>
2025-09-10 12:48:22 -0400

    created seperate code to create final func image mask if not found

--- whifun_functions/whifun_qc_final_func_MNI.m
+++ whifun_functions/whifun_qc_final_func_MNI.m
@@ -16,34 +16,35 @@ if isempty(norm_qc_dir)
     whifun_qc_coreg_slover(template_path, [final_func_MNI,',1'], out_image_slover_path, slover_slices_mni, slover_contour_range_mni, slover_view)
 end
 
-out_image_path_vox_ts = fullfile(out_folder,'MNI_Space','Vox_ts',[name '.png']);
-[out_folder1,~,~] = fileparts(out_image_path_vox_ts);
-if ~exist(out_folder1, 'dir')
-    mkdir(out_folder1);
+if ~isempty(motion_txt)
+    out_image_path_vox_ts = fullfile(out_folder,'MNI_Space','Vox_ts',[name '.png']);
+    [out_folder1,~,~] = fileparts(out_image_path_vox_ts);
+    if ~exist(out_folder1, 'dir')
+        mkdir(out_folder1);
+    end
+    out_mask_path = fullfile(out_folder,'MNI_Space','Masks_for_Vox_ts');
+
+    norm_file = whifun_create_file(over_write,out_image_path_vox_ts);
+
+    if isempty(norm_file)
+        % Quality Control
+        % Plot the mean time series after Normalization
+
+        f = figure('visible','off');
+        GM_mask_path = GM_MNI;
+        WM_mask_path = WM_MNI;
+        CSF_mask_path = CSF_MNI;
+
+        num_erosions = 4;
+        Par_name = name;
+        whifun_ts_qc(GM_mask_path,WM_mask_path,CSF_mask_path,final_func_MNI,motion_txt,num_erosions,Par_name,over_write,f)
+        exportgraphics(f,out_image_path_vox_ts)
+
+        clf(f)
+        now_mask_path = dir(WM_mask_path);
+        deep_WM_mask_path = fullfile(now_mask_path.folder,['deep_' 'num_er-' num2str(num_erosions) '_' now_mask_path.name]);
+        whifun_ts_mask_qc(out_mask_path,GM_mask_path,WM_mask_path,deep_WM_mask_path,CSF_mask_path,final_func_MNI,name,slover_slices_mni,slover_contour_range_mni,slover_view)
+    end
 end
-out_mask_path = fullfile(out_folder,'MNI_Space','Masks_for_Vox_ts');
-
-norm_file = whifun_create_file(over_write,out_image_path_vox_ts);
-
-if isempty(norm_file)
-    % Quality Control
-    % Plot the mean time series after Normalization
-
-    f = figure('visible','off');
-    GM_mask_path = GM_MNI;
-    WM_mask_path = WM_MNI;
-    CSF_mask_path = CSF_MNI;
-
-    num_erosions = 4;
-    Par_name = name;
-    whifun_ts_qc(GM_mask_path,WM_mask_path,CSF_mask_path,final_func_MNI,motion_txt,num_erosions,Par_name,over_write,f)
-    exportgraphics(f,out_image_path_vox_ts)
-
-    clf(f)
-    now_mask_path = dir(WM_mask_path);
-    deep_WM_mask_path = fullfile(now_mask_path.folder,['deep_' 'num_er-' num2str(num_erosions) '_' now_mask_path.name]);
-    whifun_ts_mask_qc(out_mask_path,GM_mask_path,WM_mask_path,deep_WM_mask_path,CSF_mask_path,final_func_MNI,name,slover_slices_mni,slover_contour_range_mni,slover_view)
-end
-
 
 
