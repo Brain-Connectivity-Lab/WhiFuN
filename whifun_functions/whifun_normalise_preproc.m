56a3c1bc65864f15013144b90f5acec1770f3b08
pratik55 <40887772+pratik55@users.noreply.github.com>
2025-09-17 08:39:23 -0400

    creating anat MNI mask here. and the func mask here as well

--- whifun_functions/whifun_normalise_preproc.m
+++ whifun_functions/whifun_normalise_preproc.m
@@ -1,4 +1,4 @@
-function [Subj_list_1,out_func_path,out_anat_path] = whifun_normalise_preproc(quality_control_path,Subj_list_1,in_func_path,in_anat_path,in_def_path,vox,Norm_pre,in_wanat_mask_MNI_path,log_fileID,over_write)
+function [Subj_list_1,out_func_path,out_anat_path] = whifun_normalise_preproc(quality_control_path,Subj_list_1,in_func_path,in_anat_path,in_def_path,vox,Norm_pre,GM_path,WM_path,CSF_path,log_fileID,over_write)
 % WHIFUN_NORMALISE_PREPROC Orchestrates normalization to MNI space.
 %
 %   [Subj_list_1, out_func_path, out_anat_path] = WHIFUN_NORMALISE_PREPROC(...)
@@ -39,7 +39,53 @@ function [Subj_list_1,out_func_path,out_anat_path] = whifun_normalise_preproc(qu
 %
 %   Author: Pratik Jain
 %   See also WHIFUN_CREATE_FILE, WHIFUN_NORMALISE, TRY, CATCH.
+
 disp(['Normalization is started for ' Subj_list_1.name])
+
+disp(['Normalization is done for ' Subj_list_1.name])
+try
+    disp(['Anatomical Normalization of is started  for ' Subj_list_1.name])
+    now_anat_path = dir(in_anat_path) ;
+    out_anat_path = fullfile(now_anat_path.folder,[Norm_pre,now_anat_path.name]);
+
+    norm_a_dir = whifun_create_file(over_write,out_anat_path);
+
+    if isempty(norm_a_dir)
+        norm_op = whifun_normalise(in_def_path,'',in_anat_path,0,nan,Norm_pre,0);
+        fprintf(log_fileID,'#####################################################################################################################\n \n');
+        fprintf(log_fileID, 'Normalization of Anatomical images to MNI space\n');
+        fprintf(log_fileID,'%s',  norm_op);
+    else
+        disp(['Anatomical Normalization file found, hence skipping this step for ' Subj_list_1.name]);
+    end
+    Subj_list_1.anat_MNI = out_anat_path;
+
+    % Making anat mask in MNI Space
+    disp(['Making Anat mask in MNI Space for ' Subj_list_1.name])
+    
+    now_anat_path = dir(out_anat_path);
+    out_wanat_mask_MNI_path = fullfile(now_anat_path.folder,['anat_mask_' now_anat_path.name]);
+    wanat_mask_path = whifun_create_file(over_write, out_wanat_mask_MNI_path);
+    
+    if isempty(wanat_mask_path)
+        anat_mask_mni_op = whifun_anat_mask(out_wanat_mask_MNI_path,out_anat_path,GM_path,WM_path,CSF_path);
+        % anat_mask_mni_op = whifun_wanat_mask(now_anat_path);
+        fprintf(log_fileID,'#####################################################################################################################\n \n');
+        fprintf(log_fileID, 'Anat Mask MNI\n');
+        fprintf(log_fileID,'%s',  anat_mask_mni_op);
+    end
+    Subj_list_1.anat_mask_MNI = out_wanat_mask_MNI_path;
+catch exception
+    disp('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    disp(['Preprocessing has encountered errors in Anatomical Normalization for ' Subj_list_1.name ', I have saved the variables in the participant folder :-) '])
+    disp('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+    Subj_list_1.error = 1;                                                                                    % Remove participant from further preprocessing                             % Subj_list_all(logical(string({Subj_list_all.name}) == Subj_list_1.name)).error = 1;
+    write_error(exception,quality_control_path, Subj_list_1.name)                % write error to text file and display                % write error to text file, update csv and display
+
+    return
+end
+disp(['Anatomical Normalization is done for ' Subj_list_1.name])
 
 try
 
@@ -66,9 +112,9 @@ try
     
     out_func_path = fullfile(now_func_path.folder,[Norm_pre,now_func_path.name]);
     [fol,name,ext] = fileparts(out_func_path);
-    out_func_mni_mask_path = fullfile(fol,['wfunc_mask_' name ext]);
+    out_func_mni_mask_path = fullfile(fol,['func_mask_' name ext]);
     norm_path = whifun_create_file(over_write,out_func_mni_mask_path);
-
+    in_wanat_mask_MNI_path = out_wanat_mask_MNI_path;
     if isempty(norm_path)
         reslice_data(in_wanat_mask_MNI_path,out_func_path,1,1,out_func_mni_mask_path);
     end
@@ -86,32 +132,3 @@ catch exception
 
 end
 
-
-disp(['Normalization is done for ' Subj_list_1.name])
-try
-    disp(['Anatomical Normalization of is started  for ' Subj_list_1.name])
-    now_anat_path = dir(in_anat_path) ;
-    out_anat_path = fullfile(now_anat_path.folder,[Norm_pre,now_anat_path.name]);
-
-    norm_a_dir = whifun_create_file(over_write,out_anat_path);
-
-    if isempty(norm_a_dir)
-        norm_op = whifun_normalise(in_def_path,'',in_anat_path,0,nan,Norm_pre,0);
-        fprintf(log_fileID,'#####################################################################################################################\n \n');
-        fprintf(log_fileID, 'Normalization of Anatomical images to MNI space\n');
-        fprintf(log_fileID,'%s',  norm_op);
-    else
-        disp(['Anatomical Normalization file found, hence skipping this step for ' Subj_list_1.name]);
-    end
-    Subj_list_1.anat_MNI = out_anat_path;
-catch exception
-    disp('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-    disp(['Preprocessing has encountered errors in Anatomical Normalization for ' Subj_list_1.name ', I have saved the variables in the participant folder :-) '])
-    disp('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-
-    Subj_list_1.error = 1;                                                                                    % Remove participant from further preprocessing                             % Subj_list_all(logical(string({Subj_list_all.name}) == Subj_list_1.name)).error = 1;
-    write_error(exception,quality_control_path, Subj_list_1.name)                % write error to text file and display                % write error to text file, update csv and display
-
-    return
-end
-disp(['Anatomical Normalization is done for ' Subj_list_1.name])
