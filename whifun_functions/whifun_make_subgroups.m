cb74fc0015eed98f985ea8e096ebdd609c8189a5
pratik55 <40887772+pratik55@users.noreply.github.com>
2025-10-08 14:24:02 -0400

    added help

--- whifun_functions/whifun_make_subgroups.m
+++ whifun_functions/whifun_make_subgroups.m
@@ -1,57 +1,40 @@
-% function subgroups = whifun_make_subgroups(total_subjects, n)
-%     % make_subgroups: Divide subjects into subgroups of size n with minimal overlap
-%     % Inputs:
-%     %   total_subjects - total number of subjects (e.g., 929)
-%     %   n - subgroup size
-%     % Output:
-%     %   subgroups - cell array, each cell contains subject indices
-% 
-%     % Check inputs
-%     if n > total_subjects
-%         error('Subgroup size n must be <= total number of subjects');
-%     end
-% 
-%     % Shuffle subject indices to randomize assignment
-%     subjects = randperm(total_subjects);
-% 
-%     % Compute number of full groups
-%     num_full_groups = floor(total_subjects / n);
-%     remainder = mod(total_subjects, n);
-% 
-%     subgroups = cell(num_full_groups + (remainder > 0), 1);
-% 
-%     % Assign full groups
-%     for i = 1:num_full_groups
-%         idx_start = (i-1)*n + 1;
-%         idx_end = i*n;
-%         subgroups{i} = subjects(idx_start:idx_end);
-%     end
-% 
-%     % If remainder exists, assign leftover subjects
-%     if remainder > 0
-%         leftover = subjects(num_full_groups*n + 1:end);
-% 
-%         % To keep group size = n, fill with random subjects from earlier groups
-%         filler_needed = n - remainder;
-%         filler = randperm(num_full_groups*n, filler_needed);
-% 
-%         subgroups{end} = [leftover, subjects(filler)];
-%     end
-% end
-
 function [subgroups, overlaps] = whifun_make_subgroups(total_subjects, n)
-    % make_subgroups_with_overlap:
-    % Divide subjects into subgroups of size n with minimal overlap
-    % and record any overlaps.
-    %
-    % Inputs:
-    %   total_subjects - total number of subjects (e.g., 929)
-    %   n              - subgroup size
-    %
-    % Outputs:
-    %   subgroups - cell array, each cell contains subject indices
-    %   overlaps  - struct listing subjects that appear in multiple groups
-    
+%WHIFUN_MAKE_SUBGROUPS Creates non-overlapping and potentially one-overlapping
+%                       subgroups of subjects from a total pool.
+%
+%   [SUBGROUPS, OVERLAPS] = WHIFUN_MAKE_SUBGROUPS(TOTAL_SUBJECTS, N)
+%
+%   This function partitions a set of 'total_subjects' into subgroups of
+%   size 'n'. If the total number of subjects is not divisible by 'n',
+%   the last subgroup is formed by taking the remaining subjects and
+%   "filling" the group to size 'n' by randomly sampling subjects from the
+%   previously formed full subgroups. This ensures all subgroups are of
+%   uniform size 'n', but the last group may overlap with others.
+%
+%   Input Arguments:
+%   TOTAL_SUBJECTS - The total number of subjects available (e.g., number of rows in a data table).
+%   N              - The desired size of each subgroup (number of subjects per group).
+%
+%   Output Arguments:
+%   SUBGROUPS      - A cell array where each cell contains a vector of subject
+%                    indices (1 to TOTAL_SUBJECTS) forming a subgroup of size 'n'.
+%   OVERLAPS       - A structure detailing the subjects involved in an overlap
+%                    (i.e., subjects who are in the last group and one of the
+%                    previous groups).
+%       .subjects  - A vector of subject indices that appear in more than one subgroup.
+%       .groups    - A cell array where OVERLAPS.groups{i} lists the subgroup
+%                    indices (1, 2, ...) that OVERLAPS.subjects(i) belongs to.
+%
+%   Example:
+%      [groups, overlap_info] = whifun_make_subgroups(10, 3);
+%      % groups might be: {[ 6 3 7], [ 8 5 1], [ 2 4 9], [10 3 4]} - where
+%      3, 4 were repeated
+%      % overlap_info will contain information on which subjects (3, 4)
+%      % were repeated.
+%
+%   Author: Pratik Jain
+
+
     % ---- Check inputs ----
     if n > total_subjects
         error('Subgroup size n must be <= total number of subjects');
