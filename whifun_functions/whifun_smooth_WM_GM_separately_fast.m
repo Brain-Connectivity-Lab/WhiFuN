6df1608bfdc16a22bfbca256e40ff7b99d16cad5
pratik55 <40887772+pratik55@users.noreply.github.com>
2026-06-10 10:24:27 -0400

    update with the whifun_cast

--- whifun_functions/whifun_smooth_WM_GM_separately_fast.m
+++ whifun_functions/whifun_smooth_WM_GM_separately_fast.m
@@ -114,7 +114,7 @@ for i=1:func_info.ImageSize(4)    % go over all timepoints (volumes)
     smoothed_WM_data(:,:,:,i) = curr_volume_data;
 end
 clear func_ma curr_volume_data WM_image;
-final_func_matrix = cast(((smoothed_GM_data + smoothed_WM_data)-func_info.AdditiveOffset)/func_info.MultiplicativeScaling,func_info.Datatype);    % combining the GM and WM images
+[final_func_matrix,func_info.AdditiveOffset,func_info.MultiplicativeScaling] = whifun_cast(((smoothed_GM_data + smoothed_WM_data)),func_info.Datatype);    % combining the GM and WM images
 niftisave(final_func_matrix,fullfile(now_func_path.folder,[smooth_pre now_func_path.name]),func_info);
 
 % deleting the old WM/GM-only functional files
